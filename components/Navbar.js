4eea17f11d2ab89144d1f8e9ae8ba07cf0187bc3
Tauqeer <hellotauqeer@gmail.com>
2023-05-01 02:21:58 +0530

    added image and its code

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image';
+import avatar from '../temp/avatar.png'
 
 const style = {
     wrapper : `h-16 w-full bg-black text-white flex md:justify-around items-center px-60`
@@ -10,7 +12,16 @@ export default function Navbar() {
       <div className={style.leftMenu}>
         <div className={style.logo}>
             <p>Uber</p>
+            <div className="style menuItem">Ride</div>
+            <div className="style menuItem">Drive</div>
+            <div className="style menuItem">More</div>
         </div>
+      </div>
+      <div className={style.rightMenu}>
+      <div className={style.menuItem}>Help</div>
+      <div className={style.menuItem}>BooMName</div>
+      <div className={style.userImageContainer}></div>
+<Image src={avatar} width={40} height={40}/>
       </div>
     </div>
   )
