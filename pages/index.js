82d3b65f43957c1b9c466bf1843b141e83a4704c
Tauqeer <hellotauqeer@gmail.com>
2023-05-01 02:07:09 +0530

    added div for style

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Navbar from '../components/Navbar'
-import React from 'react'
+import Navbar from "../components/Navbar";
+import React from "react";
 
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`,
+};
 export default function Home() {
   return (
     <div>
-      
+      <div className={style.wrapper}>
+        <Navbar></Navbar>
+        <div className={style.main}></div>
+        {/* map */}
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}></div>
+      </div>
     </div>
-  )
+  );
 }
