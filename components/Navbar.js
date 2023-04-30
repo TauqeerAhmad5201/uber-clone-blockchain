277bdbe415eaae25720c435b016cfe9eb3a9ac8d
Tauqeer <hellotauqeer@gmail.com>
2023-05-01 02:05:43 +0530

    added black navbar base

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,17 @@
-const Nabar = () => {
-    return <div>Navbar</div>
+import React from 'react'
+
+const style = {
+    wrapper : `h-16 w-full bg-black text-white flex md:justify-around items-center px-60`
 }
 
-export default Navbar
+export default function Navbar() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.leftMenu}>
+        <div className={style.logo}>
+            <p>Uber</p>
+        </div>
+      </div>
+    </div>
+  )
+}
